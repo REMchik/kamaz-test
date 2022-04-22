0b25f762d3312cf7794cbe06983f2e94c233d33e
Roman Trippel <roman_rem@mail.ru>
2022-04-22 21:53:28 +0300

    Добавлен шаблон проекта

---
+++ src/main/java/com/example/kamaz/demo/KamazDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.kamaz.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamazDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamazDemoApplication.class, args);
+    }
+
+}
